a7f8826e2133bd687a08b959c206fa6f35cc0e4c
Nils Ratusznik <nils.github@anotherhomepage.org>
2024-10-13 19:55:22 +0200

    Add dss download and install script.

---
+++ download_and_install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+dss_user="dss"
+home_dir="/home/${dss_user}"
+dss_url="https://cdn.downloads.dataiku.com/public/dss/11.3.2/dataiku-dss-11.3.2.tar.gz"
+dss_data_dir="${home_dir}/DSS_DATA_DIR"
+
+wget -q "${dss_url}" "${home_dir}/dss.tar.gz"
+mkdir "${dss_data_dir}"
+tar xf "${home_dir}/dss.tar.gz" -C ${home_dir} && chown -R ${dss_user}:${dss_user} ${home_dir}/dataiku-dss-11.3.2 && rm -f ${home_dir}/dss.tar.gz
+su - ${dss_user} -c '${home_dir}/dataiku-dss-11.3.2/installer.sh -d ${dss_data_dir} -p 11000'
