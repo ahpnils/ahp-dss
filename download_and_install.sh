aeeedae1d0b3245b839e6443d085e9283b6bbcf7
Nils Ratusznik <nils.github@anotherhomepage.org>
2024-12-16 22:55:09 +0100

    Fix install directory owner and protect variables.

--- download_and_install.sh
+++ download_and_install.sh
@@ -6,6 +6,6 @@ dss_url="https://cdn.downloads.dataiku.com/public/dss/11.3.2/dataiku-dss-11.3.2.
 dss_data_dir="${home_dir}/DSS_DATA_DIR"
 
 wget -q "${dss_url}" -O "${home_dir}/dss.tar.gz"
-mkdir -p "${dss_data_dir}"
-tar xf "${home_dir}/dss.tar.gz" -C ${home_dir} && chown -R ${dss_user}:${dss_user} ${home_dir}/dataiku-dss-11.3.2 && rm -f ${home_dir}/dss.tar.gz
-su - ${dss_user} -c '${home_dir}/dataiku-dss-11.3.2/installer.sh -d ${dss_data_dir} -p 11000'
+install -d -o "${dss_user}" -g "${dss_user}" "${dss_data_dir}"
+tar xf "${home_dir}/dss.tar.gz" -C "${home_dir}" && chown -R "${dss_user}:${dss_user}" "${home_dir}/dataiku-dss-11.3.2" && rm -f "${home_dir}/dss.tar.gz"
+su - "${dss_user}" -c "${home_dir}/dataiku-dss-11.3.2/installer.sh -d ${dss_data_dir} -p 11000"
