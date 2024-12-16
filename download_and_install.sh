574e5858d6393d316dcf9ae337dab512b43a494f
Nils Ratusznik <nils.github@anotherhomepage.org>
2024-12-16 22:31:51 +0100

    Fix download location and do not raise an issue if dss_data_dir already exists.

--- download_and_install.sh
+++ download_and_install.sh
@@ -5,7 +5,7 @@ home_dir="/home/${dss_user}"
 dss_url="https://cdn.downloads.dataiku.com/public/dss/11.3.2/dataiku-dss-11.3.2.tar.gz"
 dss_data_dir="${home_dir}/DSS_DATA_DIR"
 
-wget -q "${dss_url}" "${home_dir}/dss.tar.gz"
-mkdir "${dss_data_dir}"
+wget -q "${dss_url}" -O "${home_dir}/dss.tar.gz"
+mkdir -p "${dss_data_dir}"
 tar xf "${home_dir}/dss.tar.gz" -C ${home_dir} && chown -R ${dss_user}:${dss_user} ${home_dir}/dataiku-dss-11.3.2 && rm -f ${home_dir}/dss.tar.gz
 su - ${dss_user} -c '${home_dir}/dataiku-dss-11.3.2/installer.sh -d ${dss_data_dir} -p 11000'
